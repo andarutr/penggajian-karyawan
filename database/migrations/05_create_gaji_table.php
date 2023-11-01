cc97d5e2bee62cfc5d6a4d822aa01c8ddf403380
Andaru Triadi <andarutr@gmail.com>
2023-11-01 11:08:05 +0700

    Hello World

---
+++ database/migrations/05_create_gaji_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gaji', function (Blueprint $table) {
+            $table->id();
+            $table->integer('no_slip');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('gaji pokok');
+            $table->integer('absen');
+            $table->string('tunjangan');
+            $table->bigInteger('total gaji');
+            $table->timestamps();
+
+            // Foreign Key
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gaji');
+    }
+};
